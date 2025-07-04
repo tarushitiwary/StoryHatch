24eb963cec3a6fc7198cbd13b96e9ec520d6ddf4
Tarushi <tarushitiwary11@gmail.com>
2025-07-04 20:24:08 +0530

    Fix: Setup DATABASES with dj_database_url

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 if 'DATABASE_URL' in os.environ:
     DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback for local dev
+        conn_max_age=600
+    )
+}
